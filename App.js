309b97d7eb0d921b66458d802f32a953c26910b0
RBeci <rbelatamas@gmail.com>
2023-09-05 04:11:41 +0200

    ExpoSdk49-Debugger-Error

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  function alma() {
+    let a=1
+    a=2
+    a=3
+    a=4
+    a=5
+    a=6
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!!</Text>
       <StatusBar style="auto" />
+      <Button title='alma' onPress={()=>{alma()}}></Button>
     </View>
   );
 }
